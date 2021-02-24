bd106a89e91901cc30e05235a74933a396b8172c
avdosev <avdosev1@yandex.ru>
2021-02-24 15:21:20 +0300

    send 15:21

--- metod1.py
+++ metod1.py
@@ -1,13 +1,18 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, RidgeCV
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, BaggingRegressor, ExtraTreesRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd
 import numpy as np
 
+from catboost import CatBoostRegressor
+
 TEST_TRAIN = False
+# TEST_TRAIN = True
 
 train = pd.read_csv('./data/train.csv')
 test = pd.read_csv('./data/test.csv')
@@ -16,7 +21,7 @@ test['NTG'] = np.zeros(len(test))
 
 if TEST_TRAIN:
     X, y = train.drop(['Well', 'NTG'], axis=1), train['NTG']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 else:
     X, y = train.drop(['Well', 'NTG'], axis=1), train['NTG']
     X_train, X_test, y_train, y_test = X,X,y,y
@@ -24,12 +29,27 @@ else:
 
 # model = RandomForestRegressor(max_depth=30)
 models = [
+    (Ridge(), 'ridge'),
+    (RidgeCV(), 'ridge_cv'),
+    (make_pipeline(PolynomialFeatures(4), RidgeCV()), 'polinom 4 ridgecv'),
+    (make_pipeline(PolynomialFeatures(5), RidgeCV()), 'polinom 5 ridgecv'),
+    (make_pipeline(PolynomialFeatures(4), Ridge()), 'polinom 4 ridge'),
+    (make_pipeline(PolynomialFeatures(5), Ridge()), 'polinom 5 ridge'),
     (LinearRegression(), 'linear'),
-    (BaggingRegressor(n_jobs=-1), 'bagging'),
+    (make_pipeline(PolynomialFeatures(2), LinearRegression()), 'polinom 2 linear'),
+    (make_pipeline(PolynomialFeatures(3), LinearRegression()), 'polinom 3 linear'),
+    (make_pipeline(PolynomialFeatures(4), LinearRegression()), 'polinom 4 linear'),
+    (make_pipeline(PolynomialFeatures(5), LinearRegression()), 'polinom 5 linear'),
+    # (BaggingRegressor(n_jobs=-1), 'bagging'),
     (ExtraTreesRegressor(n_jobs=-1,), 'extra_trees'),
     (ExtraTreesRegressor(n_jobs=-1, max_depth=5), 'extra_trees max_d=5'), # 3
     (ExtraTreesRegressor(n_jobs=-1, max_depth=10), 'extra_trees max_d=10'),
     (GradientBoostingRegressor(), 'grad_boosting'),
+    (make_pipeline(PolynomialFeatures(2), GradientBoostingRegressor()), 'polinom 2 grad_boosting'),
+    (make_pipeline(PolynomialFeatures(4), GradientBoostingRegressor()), 'polinom 4 grad_boosting'),
+    (make_pipeline(PolynomialFeatures(5), GradientBoostingRegressor()), 'polinom 5 grad_boosting'),
+    (make_pipeline(PolynomialFeatures(6), GradientBoostingRegressor()), 'polinom 6 grad_boosting'),
+    (make_pipeline(PolynomialFeatures(8), GradientBoostingRegressor()), 'polinom 8 grad_boosting'),
     (RandomForestRegressor(n_estimators=25, n_jobs=-1), 'rand_forset n_est=25'),
     (RandomForestRegressor(n_estimators=10, n_jobs=-1), 'rand_forset n_est=10'),
     (RandomForestRegressor(n_estimators=5, n_jobs=-1), 'rand_forset n_est=5'),
@@ -43,6 +63,8 @@ models = [
     (KNeighborsRegressor(n_neighbors=9, weights='distance'), 'neighbors 9 distance'),
     (KNeighborsRegressor(n_neighbors=10, weights='distance'), 'neighbors 10 distance'),
     (KNeighborsRegressor(n_neighbors=15, weights='distance'), 'neighbors 15 distance'),
+    (KNeighborsRegressor(n_neighbors=20, weights='distance'), 'neighbors 20 distance'),
+    (CatBoostRegressor(silent=True, iterations=900), 'cat boost reg'),
 ]
 
 for model, name  in models:
